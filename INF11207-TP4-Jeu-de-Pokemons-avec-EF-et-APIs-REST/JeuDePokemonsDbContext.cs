e3f96835b1c0d98a4607b25e3df51c7a70bc0179
Bastien Goulet <bastien.goulet27@outlook.com>
2021-04-21 10:41:20 -0500

    Ajout de l'ORM Entity Framework

---
+++ INF11207-TP4-Jeu-de-Pokemons-avec-EF-et-APIs-REST/JeuDePokemonsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace INF11207_TP4_Jeu_de_Pokemons_avec_EF_et_APIs_REST
+{
+    public class JeuDePokemonsDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;
+                Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;
+                MultiSubnetFailover=False;Database=JeuDePokemonsDb;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
